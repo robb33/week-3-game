b446de9a3f840962095e627d28aeeb4ac051a4bd
robb33 <rbenny@earthlink.net>
2016-12-08 13:25:10 -0500

    laptop update

--- assets/javascript/psygame.js
+++ assets/javascript/psygame.js
@@ -1,9 +1,11 @@
-
 // sanity code.....keeping it simple
 
-console.log('Hello, World!')
+// console.log('Hello, World!');
 
 // pseudo code for myself
+
+
+  //code goes here
 
 
 // alphabet variable....for user input
@@ -12,59 +14,11 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-
-// variables set to zero....
-
-var wins = 0;
-var loses = 0;
-
-//variables for guessing
-var guesses = x;
-var guessesLeft = x;
-var guessedLetters = [];
-var letterToGuess = null;
-
-
-//The computer select a random letter from the var alphabet
+//Lets the computer select a random letter from the available choices
 
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-//Allows the user 10 guesses
-// guesses = guesses || 10
-
-
-
-
-// Getting HTML element and setting it =  guessesLeft. ('guessesLeft' displayed in HTML)
-
-
-
-// Here we take the guesses the user has tried -- then display it as letters separated by commas.
-
-
-
-// Function will be called when we reset everything
-
-
-
-
-//When key is released it becomes the users guess
-
-document.onkeyup = function(event) {
-
-
-
-
-	// win update & html to display the win 
-
-
-	// loss & html to display the loss 
-
-
-	// Reset & reset button or automatic....leaning on automatic
-
-
-}
+document.write(computerGuess);
 
 
 
@@ -73,5 +27,4 @@ document.onkeyup = function(event) {
 
 
 
-
-
+ 
---
+++ psyhic_pseudo.js
@@ -0,0 +1,12 @@
+
+
+//Computer randomly picks a letter from a-z
+//User randomly picks a letter from a-z
+//If there is a single letter match.  This is tallied as a win and the game resets
+//If thers isn't a match, after x amount of tries. This is tallied as a loss and the game resets
+//The user's letters are printed out on the screen, seperated by commas
+
+
+
+
+<!-- just M:€  -->
